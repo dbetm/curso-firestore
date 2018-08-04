27031ec59e635b2f247e72e028c304856c8d250d
David Betancourt Montellano <davbetm@gmail.com>
2018-08-04 16:32:35 -0500

    Lesson 6: Ordering data

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function renderCafe(doc) {
 }
 
 // Getting data
-db.collection('cafes').where('city', '==', 'Aguascalientes').get().then((snapshot) => {
+db.collection('cafes').where('city', '==', 'Fresnillo').orderBy('name').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         renderCafe(doc);
     })
